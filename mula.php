39d72fe920888cb82046f54a0d937652df79968d
Fatienirah <nurfatinumairahr@gmail.com>
2024-05-09 14:47:21 +0800

    code mula.php new

--- mula.php
+++ mula.php
@@ -1,2 +1,17 @@
-<?php
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Selamat Datang</title>
+</head>
+
+<body>
+    <h1>Selamat Datang !</h1>
+    
+    <form method="post" action="cipta_folder.php"> 
+        <div>
+            <button type="submit" name="Mula">Mula</button>
+        </div>
+    </form>
+    
+</body>
+</html>
